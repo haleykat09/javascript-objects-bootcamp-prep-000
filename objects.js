c9e5368e561bdd8f6f123d7e0175f96dd3214488
Haley West <haleykat09@yahoo.com>
2017-10-22 17:15:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = {maroon5: "Sunday Morning"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist.Beyonce = 'Crazy In Love'
+  playlist.Beyonce = 'Crazy In Love'
+
+  return playlist
 }
