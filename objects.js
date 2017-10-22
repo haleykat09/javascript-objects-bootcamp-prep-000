87027c4176353f337dedb24f783e12121b2a71e9
Haley West <haleykat09@yahoo.com>
2017-10-22 17:20:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist['Phil Ochs']
+  delete playlist['Kanye']
 
   return playlist
 }
